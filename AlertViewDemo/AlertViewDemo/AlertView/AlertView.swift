a63d2b7e05b6e0107dfab829b9bdfa35060aba55
lianggggggg <Lx754036892@outlook.com>
2018-05-14 11:20:56 +0800

    no message

---
+++ AlertViewDemo/AlertViewDemo/AlertView/AlertView.swift
@@ -0,0 +1,31 @@
+//
+//  AlertView.swift
+//  AlertViewDemo
+//
+//  Created by MrLiang on 2018/3/21.
+//  Copyright © 2018年 MrLiang. All rights reserved.
+//
+
+import UIKit
+
+class AlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    
+    
+    
+}
+
+fileprivate class AlertWindows:UIWindow{
+    
+    
+    
+}
